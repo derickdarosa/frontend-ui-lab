9f7a31aaa0aea095f2b04570010d27a76703af18
Derick Rosa <derickdr@hotmail.com>
2026-05-02 14:12:01 -0300

    Adicionado arquivos em examples, tela de login com autenticação em PHP

---
+++ examples/login-with-validation-php/loginclean.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de Login 01</title>
+    <link rel="stylesheet" href="styles.css?v=<?= time() ?>">
+</head>
+<body>
+    <header>
+        <nav class="nav">
+            <a href="/loja">Voltar para a loja</a>
+            <div class="nav-right">
+                <span>Não é membro?</span>
+                <a href="/cadastro" class="btn-outline">Entrar</a>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section class="login-box">
+            <div class="logo-tech"></div>
+
+            <form action="login.php" method="post">
+                <div class="field">
+                    <input type="email" name="email" id="email" placeholder=" " required>
+                    <label for="email">SEU EMAIL</label>
+                </div>
+                <div class="field">
+                    <input type="password" name="senha" id="senha" placeholder=" " required>
+                    <label for="senha">SUA SENHA</label>
+                </div>
+            
+                <button type="submit" value="Login">Login</button>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
